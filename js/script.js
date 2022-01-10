c8954ce0da29dc66c12846e5bbd629b7b39dd294
Aman Jena <amanjena1234@gmail.com>
2022-01-10 22:11:41 +0530

    Final changes

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 function goDown(x) {
-    console.log('scrolling')
     window.scroll({
         top: x,
         behavior: 'smooth'
@@ -10,4 +9,22 @@ function scrollToElement(e) {
     $('html,body').animate({
         scrollTop: $("." + e).offset().top - 50
     });
+}
+
+var officeInfo = {
+    'USA': {
+        'office-address': '518 144th Ave NE Redmond, WA 98052', 'office-phone': '+1 989-234 - 8892'
+    },
+    'INDIA': {
+        'office-address': 'Bhubaneswar, Odisha, PIN-751003', 'office-phone': '+91 0000-000-000'
+    }
+}
+var currentOffice = 'USA';
+
+function changeOffice(place) {
+    document.getElementById('office-address').innerHTML = officeInfo[place]['office-address'];
+    document.getElementById('office-phone').innerHTML = officeInfo[place]['office-phone'];
+    document.getElementById(place).classList.add('active');
+    document.getElementById(currentOffice).classList.remove('active');
+    currentOffice = place;
 }
